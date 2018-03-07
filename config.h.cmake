33b5db81aefce3fd8c845a315944c4c0920471f9
René Milk <rene.milk@wwu.de>
2018-03-07 19:44:52 +0100

    project init

---
+++ config.h.cmake
@@ -0,0 +1,45 @@
+/* begin dune-visibility-demo
+   put the definitions for config.h specific to
+   your project here. Everything above will be
+   overwritten
+*/
+
+/* begin private */
+/* Name of package */
+#define PACKAGE "@DUNE_MOD_NAME@"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "@DUNE_MAINTAINER@"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "@DUNE_MOD_NAME@"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "@DUNE_MOD_NAME@ @DUNE_MOD_VERSION@"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "@DUNE_MOD_NAME@"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "@DUNE_MOD_URL@"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "@DUNE_MOD_VERSION@"
+
+/* end private */
+
+/* Define to the version of dune-visibility-demo */
+#define DUNE_VISIBILITY_DEMO_VERSION "@DUNE_VISIBILITY_DEMO_VERSION@"
+
+/* Define to the major version of dune-visibility-demo */
+#define DUNE_VISIBILITY_DEMO_VERSION_MAJOR @DUNE_VISIBILITY_DEMO_VERSION_MAJOR@
+
+/* Define to the minor version of dune-visibility-demo */
+#define DUNE_VISIBILITY_DEMO_VERSION_MINOR @DUNE_VISIBILITY_DEMO_VERSION_MINOR@
+
+/* Define to the revision of dune-visibility-demo */
+#define DUNE_VISIBILITY_DEMO_VERSION_REVISION @DUNE_VISIBILITY_DEMO_VERSION_REVISION@
+
+/* end dune-visibility-demo
+   Everything below here will be overwritten
+*/
---
+++ dune/visibility-demo/visibility-demo.hh
@@ -0,0 +1,6 @@
+#ifndef DUNE_VISIBILITY_DEMO_HH
+#define DUNE_VISIBILITY_DEMO_HH
+
+// add your classes here
+
+#endif // DUNE_VISIBILITY_DEMO_HH
